e678af5b416af1dd33482d4c86920b787376b7bd
Felipe Oliveira <47610299+felipeoliveiradev@users.noreply.github.com>
2020-07-28 17:57:41 -0300

    FIX ERROR ASYNC FUNC
    how are you? try catch was missing for error message to be handled

--- src/index.js
+++ src/index.js
@@ -19,38 +19,43 @@ async function main() {
 		consumer_key: process.env.CONSUMER_KEY,
 		consumer_secret: process.env.CONSUMER_SECRET,
 	});
-
 	// Use the previous client to fetch the bearer token
 	// This method gives you an application-only token specifically for read-only access to public information.
-	const bearer = await client.getBearerToken();
+	try {
+		const bearer = await client.getBearerToken();
+		// Create a new twitter client with this token.
+		// We assign this client to a global variable so we can access it in the api module
+		globalThis.TwitterClient = new Twitter({
+			bearer_token: bearer.access_token,
+		});
 
-	// Create a new twitter client with this token.
-	// We assign this client to a global variable so we can access it in the api module
-	globalThis.TwitterClient = new Twitter({
-		bearer_token: bearer.access_token,
-	});
+		// fetch the information of the logged in user
+		// instead of getMe you could replace it with another method to get a third user to generate their circles
+		const user = await getUser("foliverofficial");
 
-	// fetch the information of the logged in user
-	// instead of getMe you could replace it with another method to get a third user to generate their circles
-	const user = await getUser("WHATEVER_USERNAME_YOU_WANT");
+		// this is how many users we will have for each layer from the inside out
+		const layers = [8, 15, 26];
 
-	// this is how many users we will have for each layer from the inside out
-	const layers = [8, 15, 26];
+		// fetch the interactions
+		const data = await getInteractions(
+			user.screen_name.toLowerCase(),
+			layers
+		);
 
-	// fetch the interactions
-	const data = await getInteractions(user.screen_name.toLowerCase(), layers);
+		// render the image
+		await render([
+			{distance: 0, count: 1, radius: 110, users: [user]},
+			{distance: 200, count: layers[0], radius: 64, users: data[0]},
+			{distance: 330, count: layers[1], radius: 58, users: data[1]},
+			{distance: 450, count: layers[2], radius: 50, users: data[2]},
+		]);
 
-	// render the image
-	await render([
-		{distance: 0, count: 1, radius: 110, users: [user]},
-		{distance: 200, count: layers[0], radius: 64, users: data[0]},
-		{distance: 330, count: layers[1], radius: 58, users: data[1]},
-		{distance: 450, count: layers[2], radius: 50, users: data[2]},
-	]);
-
-	// Look at the arguments passed to the cli. If one of them is --text then we want to render a text version of the image too
-	const shouldRenderText = process.argv.find((arg) => arg === "--text");
-	if (shouldRenderText) await renderText(data);
+		// Look at the arguments passed to the cli. If one of them is --text then we want to render a text version of the image too
+		const shouldRenderText = process.argv.find((arg) => arg === "--text");
+		if (shouldRenderText) await renderText(data);
+	} catch (err) {
+		console.log(err.errors[0]);
+	}
 }
 
 // entry point
